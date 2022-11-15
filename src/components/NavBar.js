690a968679823f5b9770f39a113ca38a006eb3e7
Mejor <amujobabatunde7@gmail.com>
2022-11-16 00:24:51 +0100

    style: made the nav bar responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,28 +3,39 @@ import { Link } from 'react-router-dom';
 import metabnbIcon from '../assests/metabnb-header.png'
 import '../styles/NavBar.css'
 import ConnectWallet from './ConnectWallet';
+import iconHamburger from '../assests/icon-hamburger.svg'
+import close from '../assests/close.svg'
 const NavBar = () => {
-    const [walletModal,setWalletModal] = useState(false)
-    
+    const [walletModal, setWalletModal] = useState(false)
+    const [mobileDisplayer, setMobileDisplayer]= useState(false)
+
     return (<>
         <div className='nav-bar-wrapper'>
             <img src={metabnbIcon} alt="" />
-     
+            <img src={iconHamburger} className='mobile_displayer mobile_displayer-hide' onClick={()=>setMobileDisplayer(prev=>!prev)} alt=''/>
+        
+
+            <div className='mobile_nav'>
+                <img src={ close} className='mobile_displayer-hide closing_icon'  alt='' onClick={()=>setMobileDisplayer(false)}/>
                 <nav className='nav-link-container'>
                     <Link to={'/'}>Home</Link>
-                    <Link to={'/place_to_stay'}>Place to stay</Link>                 
+                    <Link to={'/place_to_stay'}>Place to stay</Link>
                     <Link>NFTs</Link>
                     <Link>Community</Link>
                 </nav>
 
-           
-            <button id='connect_wallet_btn' onClick={()=>setWalletModal(prev=>!prev)}>Connect wallet</button>
-           
-             <div className={`${walletModal?'wallet_modal':'hidden'}`}>
-                <ConnectWallet setWalletModal={ setWalletModal} />
-            </div>  
+
+                <button id='connect_wallet_btn' onClick={() => setWalletModal(prev => !prev)}>Connect wallet</button>
+
+            </div>
+
+
+
+            <div className={`${walletModal ? 'wallet_modal' : 'hidden'}`}>
+                <ConnectWallet setWalletModal={setWalletModal} />
+            </div>
         </div>
     </>);
 }
- 
+
 export default NavBar;
