9c907d9e857cf3f04a1d79e9e1f784092b538313
Mejor <amujobabatunde7@gmail.com>
2022-11-16 00:51:20 +0100

    feat: made all component responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,16 +7,24 @@ import iconHamburger from '../assests/icon-hamburger.svg'
 import close from '../assests/close.svg'
 const NavBar = () => {
     const [walletModal, setWalletModal] = useState(false)
-    const [mobileDisplayer, setMobileDisplayer]= useState(false)
+    const [mobileDisplayer, setMobileDisplayer] = useState(false)
+
+    // this function helps display the mobile mode
+    function handleMobileDisplay(){
+        return {
+            right: mobileDisplayer?'0px':'-100vw'
+        }
+    }
 
     return (<>
         <div className='nav-bar-wrapper'>
-            <img src={metabnbIcon} alt="" />
+            <img className='metabnbIconn' src={metabnbIcon} alt="" />
             <img src={iconHamburger} className='mobile_displayer mobile_displayer-hide' onClick={()=>setMobileDisplayer(prev=>!prev)} alt=''/>
         
 
-            <div className='mobile_nav'>
-                <img src={ close} className='mobile_displayer-hide closing_icon'  alt='' onClick={()=>setMobileDisplayer(false)}/>
+            <div style={handleMobileDisplay()} className='mobile_nav'>
+                <img src={close} className='mobile_displayer-hide closing_icon' alt='' onClick={() => setMobileDisplayer(false)} />
+                
                 <nav className='nav-link-container'>
                     <Link to={'/'}>Home</Link>
                     <Link to={'/place_to_stay'}>Place to stay</Link>
